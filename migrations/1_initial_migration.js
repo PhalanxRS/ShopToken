dbb76b33fcba8c1df2160c25434b227367779707
PhalanxRS <vcgens2@gmail.com>
2021-07-02 13:18:39 +0300

    Flattened Contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,5 @@
-const MyToken = artifacts.require("../Contracts/Token.sol");
+const MyToken = artifacts.require("../Contracts/TokenFlat2.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(MyToken, "Phalanx The Best", "PTB", 10**15);};
+  deployer.deploy(MyToken, "Phalanx The Best", "PTB", 10**15);
+};
