bf9ba31b29d87eb754c6c09cc45e1df2ac87b238
PhalanxRS <vcgens2@gmail.com>
2021-07-02 13:23:29 +0300

    Fixed Migration naming

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-const MyToken = artifacts.require("../Contracts/TokenFlat2.sol");
+const MyToken = artifacts.require("../Contracts/Token.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(MyToken, "Phalanx The Best", "PTB", 10**15);
